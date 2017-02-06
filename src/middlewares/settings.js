a08a8945609449c7ab6048043b4ed50b6b4a920c
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-02-06 16:48:00 +0100

    Fix redis usage

--- src/middlewares/settings.js
+++ src/middlewares/settings.js
@@ -1,13 +1,33 @@
 module.exports = ({ redis }) => async ({ request, session }) => {
   request.settings = async (key, value) => {
-    if (value === undefined) {
-      if (value === null) {
-        await this.redis.del(`SETTINGS:${session.user.userId}:${key}`);
+    return new Promise((resolve, reject) => {
+      if (value === undefined) {
+        if (value === null) {
+          redis.del(`SETTINGS:${session.user.userId}:${key}`, (err) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve();
+            }
+          });
+        } else {
+          redis.set(`SETTINGS:${session.user.userId}:${key}`, value, (err) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve();
+            }
+          });
+        }
       } else {
-        await this.redis.set(`SETTINGS:${session.user.userId}:${key}`, value);
+        redis.get(`SETTINGS:${session.user.userId}:${key}`, (err, value) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(value);
+          }
+        });
       }
-    } else {
-      return await this.redis.get(`SETTINGS:${session.user.userId}:${key}`);
-    }
+    });
   };
 };
