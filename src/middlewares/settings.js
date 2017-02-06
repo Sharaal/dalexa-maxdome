258f5262f7bd48027c1c91a89e114d22fcfe49a4
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-02-06 16:51:02 +0100

    Fix settings set/get/del

--- src/middlewares/settings.js
+++ src/middlewares/settings.js
@@ -2,6 +2,14 @@ module.exports = ({ redis }) => async ({ request, session }) => {
   request.settings = async (key, value) => {
     return new Promise((resolve, reject) => {
       if (value === undefined) {
+        redis.get(`SETTINGS:${session.user.userId}:${key}`, (err, value) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(value);
+          }
+        });
+      } else {
         if (value === null) {
           redis.del(`SETTINGS:${session.user.userId}:${key}`, (err) => {
             if (err) {
@@ -19,14 +27,6 @@ module.exports = ({ redis }) => async ({ request, session }) => {
             }
           });
         }
-      } else {
-        redis.get(`SETTINGS:${session.user.userId}:${key}`, (err, value) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(value);
-          }
-        });
       }
     });
   };
